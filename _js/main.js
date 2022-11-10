6ce1f8a961bf363ac45ffcd53a11cbec99a5d410
Gustavo Schimite Soares <gustavoschimitesoares0211@gmail.com>
2022-11-09 22:03:53 -0300

    Projeto 90%

--- _js/main.js
+++ _js/main.js
@@ -1,11 +1,11 @@
-const URL_API = 'https://zoo-animal-api.herokuapp.com/animals/rand/6';
+const URL_API = "https://zoo-animal-api.herokuapp.com/animals/rand/6";
+const LISTA_ANIMALS = "lista_animals";
 
 $(document).ready(() => {
-
   $("#btn-home").ready((e) => {
     getPagina("home.html", "main");
   });
-  
+
   $("#btn-galeria").click((e) => {
     $("#destaque").hide();
     getPagina("galeria.html", "main");
@@ -23,41 +23,80 @@ $(document).ready(() => {
 });
 
 const getAnimal = () => {
-  getPagina('galeria.html','main');
+  getPagina("galeria.html", "main");
   $.ajax({
-      url: URL_API,
-      dataType: 'json',
-      success: (data) => {
-          let listAnimal = document.createElement('div');
-          $(listAnimal).addClass('row');
-          $('#getAnimal').html(listAnimal);
+    url: URL_API,
+    dataType: "json",
+    success: (data) => {
+      let listAnimal = document.createElement("div");
+      $(listAnimal).addClass("row");
+      $("#getAnimal").html(listAnimal);
+      listaAnimals = new Array();
+      data.forEach((p, i) => {
+        let li = document.createElement("div");
+        let card = document.createElement("div");
+        let cardHeader = document.createElement("div");
+        let cardBody = document.createElement("div");
 
-          data.forEach((p, i) => {
-              let li = document.createElement('div');
-              let card = document.createElement('div');
-              let cardHeader = document.createElement('div');
-              let cardBody = document.createElement('div');
-              let img = document.createElement('img');
-              $(li).addClass('col-4');
-              $(card).addClass('card');
-              $(card).addClass('hvr-grow');
-              $(img).addClass('card-img-topo');
-              $(cardHeader).addClass('card-header');
-              $(cardBody).addClass('card-body');
+        $(li).addClass("col-4");
+        $(li).addClass("hvr-grow");
+        $(card).addClass("card");
+        $(cardHeader).addClass("card-header");
+        $(cardBody).addClass("card-body");
 
-              $(cardHeader).attr('id', `ch-pkm${i}`);
-              
-              $(cardHeader).html(`<h1>${p.name}</h1>`);
-              $(cardBody).html(`<img src='${p.image_link} '>`);
-              
-              $(card).append(cardHeader)
-                     .append(cardBody);
-              $(li).append(card);                     
-              $(listAnimal).append(li);
-          });
-      }
+        $(cardBody).attr("id", `${p.id}`);
+
+        $(cardHeader).html(`<h1>${p.name}</h1>`);
+        $(cardBody).html(`<img src='${p.image_link} '>`);
+
+        $(card).append(cardHeader).append(cardBody);
+        $(li).append(card);
+        $(listAnimal).append(li);
+        $(cardBody).click(() => {
+          console.log(`${p.id}`);
+
+          let msg = `*** ADDITIONAL INFORMATIONS ***
+Name: ${p.name}
+Scientific Name: ${p.latin_name}               
+Type: ${p.animal_type}
+Length min: ${p.length_min}
+Length max: ${p.length_max}
+Weight min: ${p.weight_min}
+Weight max: ${p.weight_max}
+Lifespan: ${p.lifespan}
+Habitat: ${p.habitat}
+Diet: ${p.diet}
+Region: ${p.geo_range}`;
+          alert(msg);
+        });
+
+        animal = {
+          id: p.id,
+          name: p.name,
+          animal_type: p.animal_type,
+          length_min: p.length_min,
+          length_max: p.length_max,
+          weight_min: p.weight_min,
+          weight_max: p.weight_max,
+          lifespan: p.lifespan,
+          habitat: p.habitat,
+          diet: p.diet,
+          geo_range: p.geo_range,
+        };
+        listaAnimals.push(animal);
+      });
+      salvarAnimal(listaAnimals);
+    },
   });
-}
+
+  // const buscarAnimal(){
+
+  // }
+
+  const salvarAnimal = (listaAnimals) => {
+    setJsonItem(LISTA_ANIMALS, listaAnimals);
+  };
+};
 /*
 var getPagina = (page, target) => {
   $.ajax({
