8d8424a071c0d0ea4d804a61b2e91aef4bca934e
patrickrweber <patrickrenanweber@gmail.com>
2022-11-09 21:00:35 -0300

    API

---
+++ _js/library.js
@@ -0,0 +1,14 @@
+/* # Confs */
+var scripts = $('#scripts').clone();
+
+
+/* # Functions*/
+const getPagina = (url, target) => {
+    $.ajax({
+        url: `_html/${url}`,
+        dataType: 'html',
+        success: (pagina) => {
+            $(target).html(pagina);
+        }
+    });
+}
